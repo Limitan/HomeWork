1a95b39f68e60f88a3e659e11360e7c1aea3e40d
Limitan <84225508+Limitan@users.noreply.github.com>
2022-02-10 19:55:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class Programm
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 9;
+            int c = b - a;
+            Console.WriteLine($"{a}{b}{c}");
+        }
+    }
+}
