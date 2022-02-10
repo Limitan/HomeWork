1f64f14b8d11bd8e0240e3932afe7cea8a006879
Limitan <84225508+Limitan@users.noreply.github.com>
2022-02-10 19:49:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        string[] nums = Console.ReadLine().Split(' ');
+        int a = int.Parse(nums[0]);
+        int b = int.Parse(nums[1]);
+        int c = int.Parse(nums[2]);
+        if (a * b == c)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
