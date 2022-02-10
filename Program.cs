df99ebafc947b35dbb8bea5b4b09feee5b1c4497
Limitan <84225508+Limitan@users.noreply.github.com>
2022-02-10 19:54:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            double n = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine(n * n);
+        }
+    }
+}
