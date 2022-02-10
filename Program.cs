87db67b19e81f2956878d081ed8f581f1fc0302d
Limitan <84225508+Limitan@users.noreply.github.com>
2022-02-10 19:53:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int summ = 0;
+            if (a > 0)
+            {
+                for (int i = 0; i < a + 1; i++)
+                {
+                    summ += i;
+                }
+            }
+            else
+            {
+                for (;a <= 1; a++)
+                {
+                    summ += a;
+                }
+            }
+            Console.WriteLine(summ);
+        }
+    }
+}
