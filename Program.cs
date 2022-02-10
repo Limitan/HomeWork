d3b2007cdc112f5557af8aed15c78804f3eef99a
Limitan <84225508+Limitan@users.noreply.github.com>
2022-02-10 19:51:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+class Programm
+{
+    static void Main()
+    {
+        string[] nums = Console.ReadLine().Split(' ');
+        int a = int.Parse(nums[0]);
+        int b = int.Parse(nums[1]);
+        Console.WriteLine(a + b);
+    }
+}
